043837347a417e87d2b59121f2b59cf1743cabb3
SebJVidal <seb.vidal@btinternet.com>
2021-06-02 20:20:49 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Popflash_Admin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.historyTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.alwaysOnTopCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.autoCopyCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(679, 20);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            this.inputTextBox.Enter += new System.EventHandler(this.InputOutputTextBox_Enter);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextBox.Location = new System.Drawing.Point(12, 39);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(679, 20);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Enter += new System.EventHandler(this.InputOutputTextBox_Enter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.historyTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(759, 383);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "History";
+            // 
+            // historyTextBox
+            // 
+            this.historyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.historyTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.historyTextBox.Location = new System.Drawing.Point(7, 20);
+            this.historyTextBox.Multiline = true;
+            this.historyTextBox.Name = "historyTextBox";
+            this.historyTextBox.ReadOnly = true;
+            this.historyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.historyTextBox.Size = new System.Drawing.Size(746, 355);
+            this.historyTextBox.TabIndex = 0;
+            this.historyTextBox.WordWrap = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearButton.Location = new System.Drawing.Point(697, 11);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // copyButton
+            // 
+            this.copyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyButton.Location = new System.Drawing.Point(697, 37);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 4;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // alwaysOnTopCheckBox
+            // 
+            this.alwaysOnTopCheckBox.AutoSize = true;
+            this.alwaysOnTopCheckBox.Location = new System.Drawing.Point(8, 19);
+            this.alwaysOnTopCheckBox.Name = "alwaysOnTopCheckBox";
+            this.alwaysOnTopCheckBox.Size = new System.Drawing.Size(98, 17);
+            this.alwaysOnTopCheckBox.TabIndex = 5;
+            this.alwaysOnTopCheckBox.Text = "Always On Top";
+            this.alwaysOnTopCheckBox.UseVisualStyleBackColor = true;
+            this.alwaysOnTopCheckBox.CheckedChanged += new System.EventHandler(this.AlwaysOnTopCheckBox_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.autoCopyCheckBox);
+            this.groupBox2.Controls.Add(this.alwaysOnTopCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 455);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(760, 65);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // autoCopyCheckBox
+            // 
+            this.autoCopyCheckBox.AutoSize = true;
+            this.autoCopyCheckBox.Location = new System.Drawing.Point(8, 43);
+            this.autoCopyCheckBox.Name = "autoCopyCheckBox";
+            this.autoCopyCheckBox.Size = new System.Drawing.Size(75, 17);
+            this.autoCopyCheckBox.TabIndex = 6;
+            this.autoCopyCheckBox.Text = "Auto Copy";
+            this.autoCopyCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 532);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "View Smoke";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox historyTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.CheckBox alwaysOnTopCheckBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox autoCopyCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Popflash_Admin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string input = inputTextBox.Text;
+            string pattern = "(setpos( -?\\d\\.?\\d){3}; setang( -?\\d\\.?\\d){3})";
+
+            Match match = Regex.Match(input, pattern);
+
+            if (match.Success)
+            {
+                string[] setpos = inputTextBox.Text.Split(';');
+                string[] elements = setpos[0].Split(' ');
+
+                string zString;
+                double zValue;
+
+                if (elements.Length >= 3)
+                {
+                    zString = elements[3];
+
+                    if (double.TryParse(zString, out zValue))
+                    {
+                        zValue -= 64.0;
+
+                        string outputText = "alias \"specsmoke\" \"" + inputTextBox.Text.Replace(" " + zString + "; setang",
+                                                                                                  " " + zValue.ToString() + "; setang") + "\"";
+
+                        outputTextBox.Text = outputText;
+                        historyTextBox.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + outputText + Environment.NewLine);
+
+                        outputTextBox.Focus();
+
+                        if (autoCopyCheckBox.Checked)
+                        {
+                            Clipboard.SetText(outputText);
+                        };
+                    };
+                };
+            };
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Text = null;
+            outputTextBox.Text = null;
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(outputTextBox.Text);
+        }
+
+        private void AlwaysOnTopCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (alwaysOnTopCheckBox.Checked)
+            {
+                Form1.ActiveForm.TopMost = true;
+            }
+            else
+            {
+                Form1.ActiveForm.TopMost = false;
+            }
+        }
+
+        private void InputOutputTextBox_Enter(object sender, EventArgs e)
+        {
+            var textBox = (TextBox)sender;
+
+            BeginInvoke((Action)textBox.SelectAll);
+        }
+    }
+}
